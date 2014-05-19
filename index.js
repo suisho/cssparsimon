5ae1b3478f41fc5a83f1d540151b27fc5cca89a7
suisho <suisho000000@gmail.com>
2014-05-20 01:39:30 +0900

    puzzle...

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ var Parsimmon = require("parsimmon")
 var regex = Parsimmon.regex;
 var string = Parsimmon.string;
 var lazy = Parsimmon.lazy;
+var optWhitespace = Parsimmon.optWhitespace;
 var succeed = Parsimmon.succeed
 var seq = Parsimmon.seq
 var alt = Parsimmon.alt
 var any = Parsimmon.any
 
+function lexeme(p) { return p.skip(optWhitespace); }
 
 module.exports = function(css){
-  css = ".c "
+  css = ".c+"
+  //var combinator = lexeme(regex(/[\s>~+]/))
   var combinator = regex(/[\s>~+]/)
-  var selector = regex(/(?!.*[\s>~+]).*/)
-
+  var selector = regex(/[^\s>~+]+/).map(function(x){
+    return x
+  })
   var exp = lazy(function(){
-    //return seq(selector)
     return seq(selector, combinator)
+
+    return selector
+    //return seq(selector)
+    //return alt(selector, combinator)
   })
 
 
