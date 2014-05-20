27da608f4e8699a6f01590900931a4ee6ea5eb1f
suisho <suisho000000@gmail.com>
2014-05-21 00:45:20 +0900

    add tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,40 @@
 var parser = require("../")
+var assert = require("assert")
 
-describe("foo", function(){
-  it("baz", function(){
-    console.log(parser("a b"))
+var itParsed = function(css, cb){
+  it(css, function(){
+    var result = parser(css)
+    cb(result, result.status, result.value)
+  })
+}
+
+var itStatusTrue = function(css){
+  itParsed(css, function(r,st,v){
+    assert.ok(st)
+  })
+}
+
+describe("status true", function(){
+  itStatusTrue("a")
+  itStatusTrue("a ")
+  itStatusTrue("a  ")
+  itStatusTrue("a+b")
+  itStatusTrue("a  b")
+  itStatusTrue("a b")
+  itStatusTrue("a + b")
+  itStatusTrue("a +b")
+  itStatusTrue("a+ b")
+  itStatusTrue("a+b~c")
+})
+
+describe("combinator", function(){
+  itParsed("a>b", function(r,st,v){
+    assert.equal(v[0].combinator, ">")
+    assert.equal(v[1].combinator, null)
+  })
+  itParsed("a b", function(r,st,v){
+    console.log(v)
+    assert.equal(v[0].combinator, " ")
+    assert.equal(v[1].combinator, null)
   })
 })
