5762a852e975ed2cf5ed6779ffe8192fd96deb96
suisho <suisho000000@gmail.com>
2014-05-21 00:12:16 +0900

    wip

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Parsimmon = require("parsimmon")
 
+// Parsimmon
 var regex = Parsimmon.regex;
 var string = Parsimmon.string;
 var lazy = Parsimmon.lazy;
@@ -9,19 +10,35 @@ var seq = Parsimmon.seq
 var alt = Parsimmon.alt
 var any = Parsimmon.any
 
+var lsqBlanket = lexeme(string("["))
+var rsqBlanket = lexeme(string("]"))
+
+function dbg(x){
+  console.log("debug:" , x)
+  return x
+}
+
 function lexeme(p) { return p.skip(optWhitespace); }
 
 module.exports = function(css){
-  css = ".c+"
+  function combinatorSep(parser) {
+    var combiParser = regex(/([\s>~+])/m).then(parser).many()
+    return seq(parser, combiParser).map(function(results) {
+      // Flatten array
+      return [results[0]].concat(results[1]);
+    }).or(succeed([]));
+  }
+  css = ".cc>a+b"
   //var combinator = lexeme(regex(/[\s>~+]/))
-  var combinator = regex(/[\s>~+]/)
-  var selector = regex(/[^\s>~+]+/).map(function(x){
-    return x
-  })
-  var exp = lazy(function(){
-    return seq(selector, combinator)
+  //var combinator = regex(/[\s>~+]/).map(dbg)
+  var selector = regex(/[^\s>~+]+/).map(dbg)
+  var atom = combinatorSep(selector)
 
-    return selector
+  var exp = lazy(function(){
+    return atom
+    //return seq(selector, combinator)
+
+    //return selector
     //return seq(selector)
     //return alt(selector, combinator)
   })
