fbfb6b2aeac2fc67f80b1a8cedae6fbd2f75cb79
suisho <suisho000000@gmail.com>
2014-05-27 00:04:29 +0900

    fix symbols usage

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var regex = Parsimmon.regex;
 var string = Parsimmon.string;
 var lazy = Parsimmon.lazy;
 var optWs = Parsimmon.optWhitespace;
+var ws = Parsimmon.whitespace;
 var succeed = Parsimmon.succeed
 var seq = Parsimmon.seq
 var alt = Parsimmon.alt
@@ -25,32 +26,38 @@ var regNot = function(re){
   return RegExp("((?!"+re.source+").)+")
 }
 
-var regOr = function(reArray){
-  return RegExp("[" + reArray.map(function(re){
-    return re.source || re
-  }).join("") + "]")
-}
-
 function not(reg){
   return regex(regNot(reg))
 }
 
-var Symbols = function(){}
-//Symbols.prototype.combinators = [">", "+", "~", "\\s"]
-Symbols.prototype.combinators = [">", "+", "~", "\\s"]
-Symbols.prototype.attrOperators = ["$"]
-var symbols = new Symbols()
-var cssparsimmon = (function(){
+// regex match chars
+var symbol = function(chars){
+  return regex(RegExp("["+chars.join("")+"]"))
+}
+// not symbols
+var othersOf = function(chars){
+  return regex(RegExp("[^"+chars.join("")+"]+"))
+}
 
+
+var cssparsimmon = (function(){
+  var combinatorSymbols = [">", "+", "~", "\\s"]
   var combinator = lazy(function(){
     var empty = optWs.then(eof).result(null)
-    var ancestory = regex(/\s+/).result(' ')
-    var othersReg = regex(regOr(symbols.combinators))
+    var ancestory = ws.result(' ')
+    var othersReg = symbol(combinatorSymbols)
     var others = seq(optWs, othersReg, optWs).map(function(r){
       return r[1]
     })
 
     return alt(empty, others, ancestory)
+  })
+
+  var element = lazy(function(){
+    var elm = othersOf(combinatorSymbols)
+    return alt(
+      seq(elm, attr.many())
+    )
   })
 
   var value = lazy(function(){
@@ -62,30 +69,24 @@ var cssparsimmon = (function(){
 
   var attr = lazy(function(){
     var attrCallParser = function(){
-      var keys = regex(/[^\]]+/)
+      var operatorSymbols = ["^", "$", "~"]
+      var keys = othersOf(operatorSymbols)
       var operators = regex(/[\^$~]?=/)
-      return alt(
-        seq(keys, operators, value).map(function(r){
-          return {
-            attr : r[0],
-            operator : r[1],
-            value : r[2]
-          }
-        }),
-        keys.map(function(r){
-          return { attr : r }
-        })
-      )
+      var keyAndValue = seq(keys, operators, value).map(function(r){
+        return {
+          attr : r[0],
+          operator : r[1],
+          value : r[2]
+        }
+      })
+      var onlyKey = keys.map(function(r){
+        return { attr : r }
+      })
+      return alt(keyAndValue, onlyKey)
     }
     return string("[").chain(attrCallParser).skip(string("]"))
   })
 
-  var element = lazy(function(){
-    var elm = not(regOr(symbols.combinators))
-    return alt(
-      seq(elm, attr.many())
-    )
-  })
 
   // endpoint
   var selector = lazy(function(){
